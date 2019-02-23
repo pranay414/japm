c62036653198f7be75e0665156b790132bf69134
Pranay Ankit <pranayankit414@gmail.com>
2019-02-23 11:34:35 +0530

    one reference to rule them all

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ import fetch from 'node-fetch';
 import semver from 'semver';
 import fs from 'fs-extra';
 
-async function fetchPackage({name, reference}) {
+async function fetchPackage({ name, reference }) {
 
     // Check for file paths
-    if([`/`, `./`, `../`].some(prefix => reference.startsWith(prefix))) {
+    if ([`/`, `./`, `../`].some(prefix => reference.startsWith(prefix))) {
         return await fs.readFile(reference);
     }
 
     // Check for version number
-    if(semver.valid(reference)) {
+    if (semver.valid(reference)) {
         return await fetchPackage({
             name,
             reference: `https://registry.yarnpkg.com/${name}/-/${name}-${reference}.tgz`,
@@ -19,9 +19,28 @@ async function fetchPackage({name, reference}) {
 
     let response = await fetch(reference);
 
-    if(!response.ok) {
+    if (!response.ok) {
         throw new Error(`Couldn't fetch package ${reference}`);
     }
 
     return await response.buffer();
+}
+
+async function getPinnedReference({ name, reference }) {
+    // semver check for packages
+    if (semver.validRange(reference) && !semver.valid(reference)) {
+        let response = await fetch(`https://registry.yarnpkg.com/${name}`);
+        let info = await response.json();
+
+        let versions = Object.keys(info.versions);
+        let maxSatisfying = semver.maxSatisfying(versions, reference);
+
+        if (maxSatisfying === null) {
+            throw new Error(`Couldn't find a version matching ${reference} for package ${name}`);
+        }
+
+        reference = maxSatisfying;
+    }
+
+    return { name, reference };
 }
